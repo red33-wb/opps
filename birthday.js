380fd35f60305134f492ef5df161dad2a17ed010
REDUAN <123816734+red33-wb@users.noreply.github.com>
2023-08-19 21:19:58 +0000

    Add files via upload

---
+++ birthday.js
@@ -0,0 +1,8 @@
+
+let btn = document.querySelector('#clk')
+
+
+btn.addEventListener('click',()=>{
+    document.querySelector('.box').style.visibility='visible'
+    document.querySelector('#clk').style.visibility='hidden' 
+})
